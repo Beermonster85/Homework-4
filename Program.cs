7225f58d480625b24014a1d44a6c5331a0c04e93
Kirill <kiril-shitikov@yandex.ru>
2023-04-21 23:03:48 +0300

    docs: added HW task #1+ task #2

--- Program.cs
+++ Program.cs
@@ -99,3 +99,74 @@
 //     numArr[i] = new Random().Next(0, 2);
 // }
 // Console.WriteLine(String.Join("", numArr));
+
+
+// Home work
+
+// Task #1
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+//Решение №1
+
+// Console.WriteLine("Введите число А");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число B");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// A = Convert.ToInt32(Math.Pow(A, B));
+// Console.WriteLine(A);
+
+
+// Решение №2 (цикл)
+
+// Console.WriteLine("Введите число А");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число B");
+// int B = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+// int y = A;
+
+// int NumPow()
+// {
+//     while (i < B)
+//     {
+//         i++;
+//         y = y * A;
+//     }
+
+//     return y;
+// }
+
+// Console.WriteLine(NumPow());
+
+
+
+// Задача №2
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Console.WriteLine("Введите число ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int i = 0;
+// int sum = 0;
+
+// int NumSum()
+// {
+//     while (number > 0)
+//     {
+//         i = number % 10;
+//         number = number / 10;
+//         sum = i + sum;
+//     }
+//     return sum;
+// }
+
+// Console.WriteLine(NumSum());
+

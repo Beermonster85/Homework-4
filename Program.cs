ff731737e884b0c8bf19da28f7f5c394352acb6e
Kirill <kiril-shitikov@yandex.ru>
2023-04-21 16:41:42 +0300

    docs: Added task #2

--- Program.cs
+++ Program.cs
@@ -1,22 +1,59 @@
-﻿// Напишите программу которая принимает на вход число А и выдает сумму чисел от 1 до А
+﻿// Задача №1
+// Напишите программу которая принимает на вход число А и выдает сумму чисел от 1 до А
 // 7 ----> 28
 // 4 ----> 10
 
-Console.WriteLine("Введите любое число А");
-int A = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-int count = 0;
+// Console.WriteLine("Введите любое число А");
+// int A = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// int count = 0;
 
-int GetSumNum()
-{
-    while (A > count)
-    {
-        count = count + 1;
-        // count++;
-        sum = sum + count;
-        // sum+ = count;
-    }
-    return sum;
-}
+// int GetSumNum()
+// {
+//     while (A > count)
+//     {
+//         count = count + 1;
+//         // count++;
+//         sum = sum + count;
+//         // sum+ = count;
+//     }
+//     return sum;
+// }
 
-Console.WriteLine(GetSumNum());
+// Console.WriteLine(GetSumNum());
+
+
+// Задача №2
+// Написать программу которая принимает на вход число и выдает на выход кол-во цифр в числе
+// 456 --->3
+// 34 -----> 2
+// 89594 ---> 5
+
+// Console.WriteLine("Ввести любое число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int count = 0;
+
+
+// int GetCountNum()
+// {
+//     while (number != 0)
+//     {
+//         number = number / 10;
+//         count++;
+//     }
+
+//     return count;
+// }
+
+// Console.WriteLine(GetCountNum());
+
+// Вариант решения №2
+// Console.WriteLine("Ввести любое число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int NumCount()
+// {
+//     return Convert.ToString(number).Length;
+// }
+
+// Console.WriteLine(NumCount());

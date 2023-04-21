708eac021f9f638bc46ed9ebc5359722a02f4e8a
Kirill <kiril-shitikov@yandex.ru>
2023-04-21 15:13:01 +0300

    docs: added task #1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿// Напишите программу которая принимает на вход число А и выдает сумму чисел от 1 до А
+// 7 ----> 28
+// 4 ----> 10
+
+Console.WriteLine("Введите любое число А");
+int A = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int count = 0;
+
+int GetSumNum()
+{
+    while (A > count)
+    {
+        count = count + 1;
+        // count++;
+        sum = sum + count;
+        // sum+ = count;
+    }
+    return sum;
+}
+
+Console.WriteLine(GetSumNum());
